c05bf3b65bc92adc635d06cb5130398786bff65b
Jutpark <jutpark@ucsc.edu>
2023-05-04 05:08:11 -0700

    weeklong pt2 done

---
+++ Player.gd
@@ -0,0 +1,49 @@
+extends KinematicBody2D
+
+
+var movespeed=500;
+var bulletspeed=1000;
+var bullet=preload("res://Bullet.tscn")
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	var motion=Vector2()
+	
+	if Input.is_action_pressed("up"):
+		motion.y-=1;
+	if Input.is_action_pressed("down"):
+		motion.y+=1;
+	if Input.is_action_pressed("left"):
+		motion.x-=1;
+	if Input.is_action_pressed("right"):
+		motion.x+=1;
+	motion=motion.normalized()
+	motion=move_and_slide(motion*movespeed)
+	look_at(get_global_mouse_position())
+	
+	if(Input.is_action_just_pressed("LMB")):
+		fire()
+	
+func fire():
+	var bullet_in=bullet.instance()
+	bullet_in.position=get_global_position()
+	bullet_in.rotation_degrees= rotation_degrees
+	bullet_in.apply_impulse(Vector2(),Vector2(bulletspeed,0).rotated(rotation))
+	get_tree().get_root().call_deferred("add_child",bullet_in)
+	
+func kill():
+	get_tree().reload_current_scene()
+	
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Area2D_body_entered(body):
+	if "Enemy" in body.name:
+		kill() # Replace with function body.
